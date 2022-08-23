40b8d91b448086ae29424147470c9a9c74ab399b
YaGrig <yashagrigorev@gmail.com>
2022-08-23 20:10:03 +0300

    feat: add cardteam component with styles

---
+++ src/components/CardTeam/team.ts
@@ -0,0 +1,9 @@
+const team = [
+    {
+        name: 'Николай Козырев',
+        photo: '../../assets/images/photo',
+        role: 'developer',
+        contribution: 'В проекте делал...',
+    },
+];
+export default team;
