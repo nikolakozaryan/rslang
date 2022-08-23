f406669796a7560da0b07456c09ea0ea690b0d3a
YaGrig <yashagrigorev@gmail.com>
2022-08-23 13:13:50 +0300

    remove console.log in Link.tsx

--- src/components/header/Navigation/Links/Link.tsx
+++ src/components/header/Navigation/Links/Link.tsx
@@ -1,12 +1,10 @@
 import React from 'react';
 import { Link } from "react-router-dom";
-import LinkProps from './interfase';
+import LinkProps from './LinkInterface';
 
-const NavLink: React.FC<LinkProps> = (prop: LinkProps) => {
-console.log(prop);
- return <>
+const NavLink: React.FC<LinkProps> = (prop: LinkProps) => <>
         <li className={prop.class}> <Link className={prop.class} to={`${prop.path}`}>{prop.page} 
         {prop.page === 'Игры' && <img src="../../assets/icons/arrowDown.svg" alt="" />}</Link></li>
-    </>}
+    </>
 
 export default NavLink;
