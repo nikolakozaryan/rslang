0479d66a2ee1256134d5b6ce4b9a1153a0508969
Nikolay Kozyrev <kolyak770@gmail.com>
2022-08-24 15:36:57 +0300

    fix: fix import pathes

--- src/components/AdvantageCard/AdvantageCard.tsx
+++ src/components/AdvantageCard/AdvantageCard.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Card from '../card/Card';
+import Card from '../Card/Card';
 import IAdvantageCard from './IAdvantageCard';
 import classes from './AdvantageCard.module.scss';
 
