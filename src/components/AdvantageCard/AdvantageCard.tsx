198550645235dcba4ae164cfefb946597af50a55
Nikolay Kozyrev <kolyak770@gmail.com>
2022-08-24 12:08:12 +0300

    fix: fix pr comments

--- src/components/AdvantageCard/AdvantageCard.tsx
+++ src/components/AdvantageCard/AdvantageCard.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import Card from '../card/Card';
 import IAdvantageCard from './IAdvantageCard';
-import styles from './AdvantageCard.module.scss';
+import classes from './AdvantageCard.module.scss';
 
 const AdvantageCard: React.FC<IAdvantageCard> = ({ imgURL, title, body }) => {
     return (
         <Card type="team">
             <img src={imgURL} alt="advantage" />
-            <h3 className={styles.title}>{title}</h3>
-            <p className={styles.body}>{body}</p>
+            <h3 className={classes.title}>{title}</h3>
+            <p className={classes.body}>{body}</p>
         </Card>
     );
 };
