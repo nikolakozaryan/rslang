15e1fa04aa7e1cbcb1cc43448e159066f2fc91bd
Nikolay <kolyak770@gmail.com>
2022-08-24 20:15:57 +0300

    fix: fix Section style

--- src/components/Section/Section.tsx
+++ src/components/Section/Section.tsx
@@ -5,7 +5,7 @@ import ISection from './SectionInterface';
 const Section = (props: ISection) => 
     <div className={classes.section}>
         <h2 className={classes.header}>{props.header} {props.advantages && <span style={{color: '#FFA733'}}>RSlang</span>}</h2>
-        <div className={classes.content}>{props.children}</div>;
+        <div className={classes.content}>{props.children}</div>
     </div>
 
 
