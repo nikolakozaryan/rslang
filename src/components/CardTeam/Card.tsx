9c708463aca50d76b1e4599eb6961562ffb9ed66
YaGrig <yashagrigorev@gmail.com>
2022-08-24 19:11:17 +0300

    fix: apply linter on card.tsx

--- src/components/CardTeam/Card.tsx
+++ src/components/CardTeam/Card.tsx
@@ -2,20 +2,20 @@ import React from 'react';
 import classes from './Card.module.scss';
 import CardTeamInterface from './CardTeamInterface';
 
-const CardTeam: React.FC<CardTeamInterface> = (props: CardTeamInterface) => 
+const CardTeam: React.FC<CardTeamInterface> = (props: CardTeamInterface) => (
     <>
-    <div className={classes.cardTeam}>
-    <div className={classes.header}>
-        <div className={classes.photo} style ={{backgroundImage: `url(${props.member.photo})`}}>
-        </div>
-        <div className={classes.description}>
-            <div className={classes.name}>{props.member.name}</div>
-            <div className={classes.role}>
-                <span>{props.member.role}</span>
+        <div className={classes.cardTeam}>
+            <div className={classes.header}>
+                <div className={classes.photo} style={{ backgroundImage: `url(${props.member.photo})` }}></div>
+                <div className={classes.description}>
+                    <div className={classes.name}>{props.member.name}</div>
+                    <div className={classes.role}>
+                        <span>{props.member.role}</span>
+                    </div>
+                </div>
             </div>
+            <div className={classes.contribution}>{props.member.contribution}</div>
         </div>
-    </div>
-    <div className={classes.contribution}>{props.member.contribution}</div>
-    </div>
     </>
+);
 export default CardTeam;
