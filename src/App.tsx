64af28537e342cf18ab34cc9ae9833339111ae98
YaGrig <76626643+YaGrig@users.noreply.github.com>
2022-08-24 19:14:24 +0300

    #8 create Header (#16)
    * add basic structure and styles to header
    
    * feat: add routes to app component, create nav component, create empty pages
    
    * feat: add styles to header, arrow to games
    
    * fix: add type to header const
    
    * refactor: check with eslint
    
    * feat: add fonts to header
    
    * feat: add hover/active to buttons in header
    
    * feat: create navLink component, insert navlink component to nav
    
    * refactor: rename linkInterface.ts file
    
    * remove console.log in Link.tsx
    
    * fix: change font of logo
    
    * fix: change font of logo
    
    * refactor: move constants to common
    
    * feat: change color palette in header
    
    * fix: fix issues with rems, folder Names, added Logo folder in Header
    
    * fix: crlf to lf
    
    * refactor: change name of folder
    
    * refactor: rename header
    
    * refactor: rename header

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,27 @@
 import React from 'react';
 import 'normalize.css';
-import './common/style/index.scss';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Main from './pages/Main/Main';
+import Header from './components/Header/Header';
+import Book from './components/Pages/Book/Book';
+import Games from './components/Pages/Games/Games';
+import Statistic from './components/Pages/Statistic/Statistic';
 
-const App = (): JSX.Element => (
-    <React.StrictMode>
+
+
+const App = (): JSX.Element =>
+    <>
+        <BrowserRouter>
+            <Header/>
+            <Routes>
+                <Route path="/book" element={<Book />} />
+                <Route path="/statistic" element={<Statistic />} />
+                <Route path="/games" element={<Games />} />
+            </Routes>
+        </BrowserRouter>
         <Main />
-    </React.StrictMode>
-);
+    </>
+
+
 
 export default App;
