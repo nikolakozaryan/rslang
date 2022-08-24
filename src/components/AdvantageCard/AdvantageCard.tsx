01a8e0d4f4ac8169f035ed01b2828c4d81318c95
nikolakozaryan <48277561+nikolakozaryan@users.noreply.github.com>
2022-08-24 20:41:29 +0300

    #21 Create Advantages Component (#30)
    * fix: unify images size
    
    * fix: remove flex from base card component
    
    * fix: fix AdvantageCard style
    
    * fix: fix Section style
    
    * feat: add Advantages component
    
    Co-authored-by: Nikolay <kolyak770@gmail.com>

--- src/components/AdvantageCard/AdvantageCard.tsx
+++ src/components/AdvantageCard/AdvantageCard.tsx
@@ -3,14 +3,12 @@ import Card from '../Card/Card';
 import IAdvantageCard from './IAdvantageCard';
 import classes from './AdvantageCard.module.scss';
 
-const AdvantageCard: React.FC<IAdvantageCard> = ({ imgURL, title, body }) => {
-    return (
-        <Card type="team">
-            <img src={imgURL} alt="advantage" />
-            <h3 className={classes.title}>{title}</h3>
-            <p className={classes.body}>{body}</p>
-        </Card>
-    );
-};
+const AdvantageCard: React.FC<IAdvantageCard> = ({ imgURL, title, body }) => (
+    <Card type="team">
+        <img className={classes.image} src={imgURL} alt="advantage" />
+        <h3 className={classes.title}>{title}</h3>
+        <p className={classes.body}>{body}</p>
+    </Card>
+);
 
 export default AdvantageCard;
