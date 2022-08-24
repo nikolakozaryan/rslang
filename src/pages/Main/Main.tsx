4ee711b046482fa58a5caaa8ed6f9b9688ad88e2
YaGrig <yashagrigorev@gmail.com>
2022-08-24 18:24:51 +0300

    fix: remove extra lines in main

--- src/pages/Main/Main.tsx
+++ src/pages/Main/Main.tsx
@@ -1,18 +1,10 @@
 import Welcome from './Welcome/Welcome';
 import Footer from '../../components/Footer/Footer';
 import Section from '../../components/Section/Section';
-import Card from '../../components/Card/Card';
-import classes from './Main.module.scss';
 
 const Main = (): JSX.Element => (
     <>
         <main>
-            <Section header={'string'} advantages ={true}>
-                <Card type = {'team'}>asd</Card>
-                <Card type = {'team'}>asd</Card>
-                <Card type = {'team'}>asd</Card>
-                <Card type = {'team'}>asd</Card>
-            </Section>
             <Welcome />
         </main>
         <Footer className={classes.footer} />
