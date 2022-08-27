f38729193356c8a4b19c48bd00d797002183c658
YaGrig <76626643+YaGrig@users.noreply.github.com>
2022-08-27 20:51:55 +0300

    #22 add team section to Main (#31)
    * fix: fix style for card
    
    * fix: add key to card
    
    * fix: remove extra tags in main

--- src/pages/Main/Main.tsx
+++ src/pages/Main/Main.tsx
@@ -1,15 +1,13 @@
 import Welcome from './Welcome/Welcome';
 import Advantages from './Advantages/Advantages';
-import Footer from '../../components/Footer/Footer';
-import classes from './Main.module.scss';
+import TeamSection from './Team/TeamSection';
 
 const Main = (): JSX.Element => (
-    <>
-        <main>
-            <Welcome />
-            <Advantages />
-        </main>
-    </>
+    <main>
+        <Welcome />
+        <Advantages />
+        <TeamSection />
+    </main>
 );
 
 export default Main;
