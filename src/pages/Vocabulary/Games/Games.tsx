7cc79fc8759a8d849f5f0bfbc6cd042090e01ef7
nikolakozaryan <48277561+nikolakozaryan@users.noreply.github.com>
2022-08-29 00:05:18 +0300

    #46 Create vocabulary games component (#52)
    * feat: add images for vocabulary games component
    
    * feat: add vocabulary games component
    
    Co-authored-by: Nikolay <kolyak770@gmail.com>

---
+++ src/pages/Vocabulary/Games/Games.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import GAME_CARDS_DATA from '../../../common/constants/gameCards';
+import Section from '../../../components/Section/Section';
+import Card from '../../../components/Card/Card';
+import classes from './Games.module.scss';
+
+const Games = () => (
+  <Section header="Игры">
+    {GAME_CARDS_DATA.map((data) => (
+      <Card type="game">
+        <div className={classes.img}>
+          <img src={data.imgURL} alt="game-image" />
+        </div>
+        <h3 className={classes.center}>{data.title}</h3>
+        <p className={classes.center}>{data.body}</p>
+      </Card>
+    ))}
+  </Section>
+);
+
+export default Games;
--- src/pages/Vocabulary/Vocabulary.tsx
+++ src/pages/Vocabulary/Vocabulary.tsx
@@ -1,15 +1,17 @@
 import React, { useState } from 'react';
 import Difficulty from './Difficulty/Difficulty';
+import Games from './Games/Games';
 import classes from './Vocabulary.module.scss';
 
 const Vocabulary = () => {
-    const [curLevel, setCurLevel] = useState(0);
-    return (
-        <>
-            <h2 className={classes.header}>Учебник</h2>
-            <Difficulty curLevel={curLevel} setCurLevel={setCurLevel} />
-        </>
-    );
+  const [curLevel, setCurLevel] = useState(0);
+  return (
+    <>
+      <h2 className={classes.header}>Учебник</h2>
+      <Difficulty curLevel={curLevel} setCurLevel={setCurLevel} />
+      <Games />
+    </>
+  );
 };
 
 export default Vocabulary;
