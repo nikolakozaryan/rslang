6761507b16ca0cc091f6a1d36f8e63c5cdcfa1e6
Kirill Naydovich <kirillnaydovich@gmail.com>
2022-09-02 13:42:29 +0300

    refactor: change indents

--- src/pages/Entrance/Entrance.tsx
+++ src/pages/Entrance/Entrance.tsx
@@ -3,12 +3,12 @@ import classes from './Entrance.module.scss';
 import RouteComponent from '../../components/RouteComponent/RouteComponent';
 
 const Entrance: React.FC<PropsWithChildren> = ({ children }) => (
-    <RouteComponent navigationHide={true}>
-        <div className={classes.container}>
-            <div className={classes.illustration}></div>
-            {children}
-        </div>
-    </RouteComponent>
+  <RouteComponent navigationHide={true}>
+    <div className={classes.container}>
+      <div className={classes.illustration}></div>
+      {children}
+    </div>
+  </RouteComponent>
 );
 
 export default Entrance;
