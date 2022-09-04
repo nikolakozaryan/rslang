84246e888dc87f88375dcf4be4c6400f2f15fe61
Kirill <57269508+OnlyR10@users.noreply.github.com>
2022-09-04 21:15:49 +0300

    #35 Create entrance (#59)
    * feat: create authorization/registration base structure
    
    * feat: add input validation
    
    * fix: change user types
    
    * feat: add user registration and authorization
    
    * feat: restrict access for unauthorized users
    
    * feat: add auto authorization of an existing user
    
    * refactor: change indents
    
    * refactor: change structure and style
    
    * refactor: merge response types
    
    * refactor: change the condition

--- src/components/Header/Navigation/Navigation.tsx
+++ src/components/Header/Navigation/Navigation.tsx
@@ -7,7 +7,7 @@ const Navbar: React.FC = () => (
   <nav>
     <ul className={classes.navList}>
       {NAVIGATION_ITEMS.map((item) => (
-        <NavLink class="navItems" page={item[0]} path={item[1]} key={item[0]} />
+        <NavLink page={item[0]} path={item[1]} key={item[0]} />
       ))}
     </ul>
   </nav>
